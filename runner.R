4e82682a1503fa5504499b9c45e4e13715fdd14f
ksonda <44071350+ksonda@users.noreply.github.com>
2020-06-30 13:45:54 -0400

    Update runner.R

--- runner.R
+++ runner.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 library(pidsvcBackup)
 library(httr)
+library(xml2)
 
-
-gather_explicit <-  
-
+batch_size=500
+root<-"https://test.geoconnex.us"
+root2<-"https://geoconnex.us"
 
 explicit <- do.call("bind_rows",
                     lapply(list.files(
-                       path="./namespaces",
+                       path="./namespaces/CHyLD",
                        pattern=".csv",
                        recursive=TRUE,
                        full.names=TRUE),
@@ -16,15 +17,26 @@ explicit <- do.call("bind_rows",
                     )
             )
 
-explicit$X1<-NULL
-                    
-            
 
-regex <- list.files(path="./namespaces",
-                    pattern=".xml",
-                    recursive=TRUE,
-                    full.names=TRUE
-                    )
+explicit <- explicit %>% mutate(grp = floor((row_number()-1)/batch_size))
+
+
+data_split <- split(explicit, list(explicit$grp))
+
+for (grp in names(data_split)){
+
+  d2 <- data_split[[grp]]
+  d2$grp<-NULL
+  write_csv(d2,path="temp.csv")
+  print(paste0(grp," is csv'd"))
+  pidsvcBackup::write_xml(in_f="temp.csv", out_f="temp.xml", root=root2)
+  print(paste0(grp," is xml'd"))
+  post_pids(in_f="temp.xml", user="iow", password="nieps", root=root)
+  print(paste0(grp," is pidsvc'd"))
+  unlink("temp.csv")
+  unlink("temp.xml")
+}
+
 
 
 
