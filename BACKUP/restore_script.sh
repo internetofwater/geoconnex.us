bd8ca5365ff69f136a476ff97bc56cd706a0fc6f
Kyle Onda <kyle.onda@duke.edu>
2020-07-27 14:21:12 -0400

    Update restore_script.sh

--- BACKUP/restore_script.sh
+++ BACKUP/restore_script.sh
@@ -4,5 +4,5 @@ for f in $FILES
 do
   echo "Posting file $f..."
   curl http://localhost:8095/pidsvc/controller?cmd=import -X POST -F "source=@$f" -H "Content-Type: multipart/mixed" 
-  cat $f
+  echo "Posted file $f"
 done  
