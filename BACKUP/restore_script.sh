2c0054271058c0c7c89e9cad4c9e787d2f79f78a
Kyle Onda <kyle.onda@duke.edu>
2020-07-27 14:20:37 -0400

    Update restore_script.sh

--- BACKUP/restore_script.sh
+++ BACKUP/restore_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-FILES=~/geoconnex.us/BACKUP/backup_current/*.xml
+FILES=~/geoconnex.us-1/BACKUP/backup_current/*.xml
 for f in $FILES
 do
   echo "Posting file $f..."
-  curl --user [name]:[password] https://geoconnex.us/pidsvc/controller?cmd=import -X POST -F "source=@$f" -H "Content-Type: multipart/mixed" 
+  curl http://localhost:8095/pidsvc/controller?cmd=import -X POST -F "source=@$f" -H "Content-Type: multipart/mixed" 
   cat $f
 done  
